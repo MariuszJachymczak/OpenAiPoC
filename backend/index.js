f3f3599b1fce1fc8203698f5ecaab4a810d96e0d
mariusz.jachymczak <mariusz.jachymczak@gazeta.pl>
2024-01-11 19:51:37 +0100

    adding logic to AI

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,36 @@
-const { OpenAI } = require("openai");
-const dotenv = require("dotenv");
+const { OpenAI } = require('openai');
+const dotenv = require('dotenv');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*'); //Allow for all origins
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  );
+  next();
+});
+
+app.listen(4000, () => {
+  console.log('Server is listening on port 4000');
+});
+
+app.post('/gpt', async (req, res) => {
+  let prompt = req.body.prompt;
+  let response = await promptAi(prompt);
+  res.send(response);
+});
+
+app.post('/dalle', async (req, res) => {
+  let prompt = req.body.prompt;
+  let response = await promptImage(prompt);
+  res.send(response);
+});
+
 dotenv.config();
 
 const configuration = {
@@ -9,24 +40,39 @@ const openai = new OpenAI(configuration);
 
 let promptAi = async (prompt) => {
   const response = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo",
+    model: 'gpt-3.5-turbo',
     messages: [
       {
-        role: "system",
+        role: 'system',
         content:
-          "You are a helpful assistant.But you are drunk. Your ansers may be a bit off.",
+          'You are a helpful assistant.But you are drunk. Your ansers may be a bit off.',
       },
       {
-        role: "user",
+        role: 'user',
         content: prompt,
       },
     ],
     temperature: 0.2,
+    frequency_penalty: 0.5,
     top_p: 1,
   });
   return response.choices[0];
 };
 
-promptAi("What is JavaScript?").then((res) => {
-  console.log(res.message);
+async function promptImage(prompt) {
+  const response = await openai.images.generate({
+    model: 'dall-e-3',
+    prompt: prompt,
+  });
+
+  // console.log(image.data);
+  image_url = response.data[0].url;
+  return image_url;
+}
+
+// promptAi("What is JavaScript?").then((res) => {
+//   console.log(res.message);
+// });
+promptImage('Pigs in space').then((res) => {
+  console.log(res);
 });
